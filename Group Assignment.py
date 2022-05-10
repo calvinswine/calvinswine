9d804b3a9aeaa994b64aa4a690ecc4ba27ad4c60
Aarush Tiwari <101958221+calvinswine@users.noreply.github.com>
2022-05-10 13:17:34 +0530

    Add files via upload

---
+++ Group Assignment.py
@@ -0,0 +1,16 @@
+T = int(input("Test cases: "))
+count = 0
+
+while(count<T):
+    n = int(input("N: "))
+    x = int(input("X: "))
+    group = list()
+    for i in range(1, (n*2)-(n-1)):
+        group.append((i, ((n*2)-(i-1))))
+    print(group)
+    for i in group:
+        if i[0] == x:
+            print("Partner of chef:", i[1])
+        elif i[1] == x:
+            print("Partner of chef:", i[0])
+    count+=1
